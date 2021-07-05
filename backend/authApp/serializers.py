c3c39a0891da54f334f113c10b7a46f48b88c3c4
Artur Rejment <arturrejment1@gmail.com>
2021-07-05 16:00:52 +0200

    Working on validating User creation

--- backend/authApp/serializers.py
+++ backend/authApp/serializers.py
@@ -9,6 +9,26 @@ class UserCreateSerializer(UserCreateSerializer):
 		model = User
 		fields = ('id', 'username', 'email', 'first_name', 'last_name', 'address')
 
+	def validate_email(self, value):
+		"""
+		Check the email format
+		"""
+		if "@" not in value:
+			raise serializers.ValidationError("Email should contain @")
+		if "." not in value:
+			raise serializers.ValidationError("Email should contain @")
+
+	def create(self, validated_data):
+		"""
+		! Create method is not working properly
+		"""
+		strip = lambda x: ut.StripAndCapital(validated_data.get(x))
+
+		validated_data['first_name'] = strip('first_name')
+		validated_data['last_name'] = strip('last_name')
+
+		return User.objects.create_user(**validated_data)
+
 class AddressSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Address
