12c509677bb1424ae373cef5dc7667597772e7f3
Artur Rejment <arturrejment1@gmail.com>
2021-07-05 22:50:26 +0200

    Working on views tests

---
+++ backend/gym/tests/test_views.py
@@ -0,0 +1,14 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from gym.models import *
+import json
+
+# class TestViews(TestCase):
+
+# 	def test_shop_view_GET(self):
+# 		client = Client()
+
+# 		response = client.get(reverse('shop'))
+# 		print(response.status_code)
+
+
