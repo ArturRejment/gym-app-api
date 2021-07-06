51aa6c25697899ab85bcd3b4b3d5be2c5cebe36b
Artur Rejment <arturrejment1@gmail.com>
2021-07-06 12:32:38 +0200

    Working on tests

--- backend/gym/tests/test_views.py
+++ backend/gym/tests/test_views.py
@@ -1,14 +1,40 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from rest_framework.test import RequestsClient, APITestCase
+from rest_framework.authtoken.models import Token
 from gym.models import *
 import json
 
-# class TestViews(TestCase):
+class TestViews(APITestCase):
 
-# 	def test_shop_view_GET(self):
-# 		client = Client()
+	# token = Token.objects.get(user__username='lauren')
 
-# 		response = client.get(reverse('shop'))
-# 		print(response.status_code)
+	def test_shop_view_GET(self):
+		# client = Client()
+
+		# client = RequestsClient()
+		headers = {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
+
+		response = self.client.post(
+			'http://127.0.0.1:8000/auth/users/',
+			{
+				'username': 'testing',
+				'email': 'test@email.com',
+				'first_name': 'test_name',
+				'last_name': 'test_lname',
+				'password': 'StroNGPass2137',
+				're-password': 'StroNGPass2137',
+				'phone': '123456789',
+				'address': '3'
+			},
+			# headers={
+			# 	'Content-Type':'application/x-www-form-urlencoded'
+			# },
+			# format='json'
+		)
+		print(response)
+
 
 
