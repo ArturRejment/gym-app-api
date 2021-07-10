894dca8df264b0a968941928cc9b0bcf800cad3c
Artur Rejment <arturrejment1@gmail.com>
2021-07-10 16:14:18 +0200

    Testing workflow

--- backend/gym/tests/test_views.py
+++ backend/gym/tests/test_views.py
@@ -1,40 +1,32 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from rest_framework.test import RequestsClient, APITestCase
+from rest_framework.test import RequestsClient, APITestCase, APIClient
 from rest_framework.authtoken.models import Token
 from gym.models import *
 import json
 
-class TestViews(APITestCase):
+# class TestViews(APITestCase):
 
-	# token = Token.objects.get(user__username='lauren')
 
-	def test_shop_view_GET(self):
-		# client = Client()
+# 	def test_shop_view_GET(self):
+# 		# client = Client()
+# 		token = Token.objects.get(user=14)
+# 		print(token)
 
-		# client = RequestsClient()
-		headers = {
-			'Content-Type': 'application/x-www-form-urlencoded'
-		}
+# 		client = APIClient()
 
-		response = self.client.post(
-			'http://127.0.0.1:8000/auth/users/',
-			{
-				'username': 'testing',
-				'email': 'test@email.com',
-				'first_name': 'test_name',
-				'last_name': 'test_lname',
-				'password': 'StroNGPass2137',
-				're-password': 'StroNGPass2137',
-				'phone': '123456789',
-				'address': '3'
-			},
-			# headers={
-			# 	'Content-Type':'application/x-www-form-urlencoded'
-			# },
-			# format='json'
-		)
-		print(response)
+# 		response = self.client.get(
+# 			'/auth/token/login',
+# 			{
+# 				'email': 'baga@gmail.com',
+# 				'password': '6unguiop'
+# 			}
+# 			# headers={
+# 			# 	'Content-Type':'application/x-www-form-urlencoded'
+# 			# },
+# 			# format='json'
+# 		)
+# 		print(response)
 
 
 
