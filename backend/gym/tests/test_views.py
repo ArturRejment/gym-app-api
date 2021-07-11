f425f7807aabc218881426005b6dc260c0de76af
Artur Rejment <arturrejment1@gmail.com>
2021-07-11 15:14:14 +0200

    Testing user creation with api

--- backend/gym/tests/test_views.py
+++ backend/gym/tests/test_views.py
@@ -2,31 +2,46 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from rest_framework.test import RequestsClient, APITestCase, APIClient
 from rest_framework.authtoken.models import Token
-from gym.models import *
+from django.contrib.auth.models import Group
+import gym.models as GymModels
+from authApp.models import Address
 import json
 
-# class TestViews(APITestCase):
+class TestViews(APITestCase):
+
+	def setUp(self):
+		self.address = Address.objects.create(
+			country='Poland',
+			city='Honolulu',
+			street='Ladna 5',
+			postcode='15-223',
+		)
+		Group.objects.create(
+			name='member'
+		)
 
 
-# 	def test_shop_view_GET(self):
-# 		# client = Client()
-# 		token = Token.objects.get(user=14)
-# 		print(token)
+	def test_shop_view_GET(self):
 
-# 		client = APIClient()
-
-# 		response = self.client.get(
-# 			'/auth/token/login',
-# 			{
-# 				'email': 'baga@gmail.com',
-# 				'password': '6unguiop'
-# 			}
-# 			# headers={
-# 			# 	'Content-Type':'application/x-www-form-urlencoded'
-# 			# },
-# 			# format='json'
-# 		)
-# 		print(response)
+		# Create new user
+		response = self.client.post(
+			'/auth/users/',
+			{
+				'username': 'test',
+				'email': 'test@gmail.com',
+				'password': 'StronGPassHeRo78423',
+				're_password': 'StronGPassHeRo78423',
+				'phone': '123456789',
+				'first_name': 'test',
+				'last_name': 'testing',
+				'address': self.address,
+			},
+			headers={
+				'Content-Type':'application/x-www-form-urlencoded'
+			},
+		)
+		# Check if user was created
+		self.assertEquals(response.status_code, 201)
 
 
 
