2aee5ce59c27f7fb8f12c9e1dfdf8672f967d75b
Artur Rejment <arturrejment1@gmail.com>
2021-07-16 13:42:05 +0200

    SetUp tests for trainer hours

---
+++ backend/people/tests/test_trainer_hours.py
@@ -0,0 +1,60 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import Group
+
+import gym.models as GymModels
+from authApp.models import Address, User
+
+class TestWorkingHours(APITestCase):
+	""" Testing Working Hours Views """
+
+	def setUp(self):
+		self.address = Address.objects.create(
+			country='Poland',
+			city='Honolulu',
+			street='Ladna 5',
+			postcode='15-223',
+		)
+		Group.objects.create(
+			name='member'
+		)
+		trainer_group = Group.objects.create(
+			name='trainer'
+		)
+		self.client.post(
+			'/auth/users/',
+			{
+				'username': 'test',
+				'email': 'test@gmail.com',
+				'password': 'StronGPassHeRo78423',
+				're_password': 'StronGPassHeRo78423',
+				'phone': '123456789',
+				'first_name': 'test',
+				'last_name': 'testing',
+				'address': self.address,
+			},
+			headers={
+				'Content-Type':'application/x-www-form-urlencoded'
+			},
+		)
+
+		client = User.objects.get(username = 'test')
+		client.groups.add(trainer_group)
+
+		token_resp = self.client.post(
+			'/auth/token/login/',
+			{
+				'email': 'test@gmail.com',
+				'password': 'StronGPassHeRo78423'
+			},
+			headers={
+				'Content-Type':'application/x-www-form-urlencoded'
+			}
+		)
+
+
+		# Save auth token
+		self.token = token_resp.data.get('auth_token')
+		# Set generated token as default authentication credentials
+		self.client.defaults['HTTP_AUTHORIZATION'] = 'Token ' + self.token
