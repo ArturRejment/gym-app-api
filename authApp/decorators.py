7dc629c0e65960f0e93ed28ff61fd7cbcb4223c7
Artur Rejment <arturrejment1@gmail.com>
2021-07-30 22:57:11 +0200

    Code reformation

--- authApp/decorators.py
+++ authApp/decorators.py
@@ -2,44 +2,43 @@ from django.http import HttpResponse
 from rest_framework import serializers
 from rest_framework.response import Response
 
+
 def allowed_users(allowed_roles = []):
+    """ Decorator function for function-based views """
+
     def decorator(view_function):
+
         def wrapper_func(request, *args, **kwargs):
-
             groups = None
-
+            # Get all user's roles, if they exists
             if request.user.groups.exists():
                 groups = request.user.groups.all()
-
+            # Check if user's role is in allowed roles
             for group in groups:
+                # If it is, return desired function (view)
                 if group.name in allowed_roles:
                     return view_function(request, *args, **kwargs)
-
             return Response({"Auth": ["You are not authorized to view the page!"]}, status=403)
-
             return view_function(request, *args, **kwargs)
-
         return wrapper_func
     return decorator
 
 def allowed_users_class(allowed_roles = []):
+    """ Decorator function for class-based views """
+
     def decorator(view_function):
+
         def wrapper_func(self, request, *args, **kwargs):
-
             group = None
-
+            # Get all user's roles, if they exists
             if request.user.groups.exists():
                 groups = request.user.groups.all()
-
-
+            # Check if user's role is in allowed roles
             for group in groups:
                 if group.name in allowed_roles:
+                # If it is, return desired function (view)
                     return view_function(self, request, *args, **kwargs)
-
             return Response({"Auth": ["You are not authorized to view the page!"]}, status=403)
-
             return view_function(self, request, *args, **kwargs)
-
-
         return wrapper_func
     return decorator
--- authApp/views.py
+++ authApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from rest_framework.response import Response
 from django.http import JsonResponse
+from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
